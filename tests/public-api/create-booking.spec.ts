d9816e7737ca12c1c67860d5bd7f4d35d337d76a
Daniel Nguyen <daniel.nguyen@inspireventures.com>
2025-11-11 19:14:30 +0700

    Create booking via Public API

--- tests/public-api/create-booking.spec.ts
+++ tests/public-api/create-booking.spec.ts
@@ -5,12 +5,20 @@ import credentials from '@/data/credentials.json';
 
 test.describe('Public API-v10: @PublicAPI ', () => {
 
-  test('[Public API] Create an IMMEDIATE booking', async ({ request }) => {
-    await createImmediateBookingPublicAPI(request, credentials.apiAuthorization['apiAuthorization-daniel-01'].apiAuthorization);
+  test('[Public API][NonBP] Create an IMMEDIATE booking', async ({ request }) => {
+    await createImmediateBookingPublicAPI(request, credentials.apiAuthorization['apiAuthorization-nonbp-17740'].apiAuthorization);
   });
 
-  test('[Public API] Create a SCHEDULE booking', async ({ request }) => {
-    await createScheduleBookingPublicAPI(request, credentials.apiAuthorization['apiAuthorization-daniel-01'].apiAuthorization);
+  test('[Public API][NonBP] Create a SCHEDULE booking', async ({ request }) => {
+    await createScheduleBookingPublicAPI(request, credentials.apiAuthorization['apiAuthorization-nonbp-17740'].apiAuthorization);
+  });
+
+  test('[Public API][BP] Create an IMMEDIATE booking', async ({ request }) => {
+    await createImmediateBookingPublicAPI(request, credentials.apiAuthorization['apiAuthorization-bp-63'].apiAuthorization);
+  });
+
+  test('[Public API][BP] Create a SCHEDULE booking', async ({ request }) => {
+    await createScheduleBookingPublicAPI(request, credentials.apiAuthorization['apiAuthorization-bp-63'].apiAuthorization);
   });
 
 });
